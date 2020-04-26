c1b3d9f393b0e1dc5a89ea1f5654af84bd6d0730
loilunhp007 <63707576+loilunhp007@users.noreply.github.com>
2020-04-26 13:36:19 +0700

    create project qltv

---
+++ qltv/src/main/java/Model/Book.java
@@ -0,0 +1,80 @@
+package Model;
+
+public class Book {
+    private int bookID,bookPrice,bookPages
+    private String bookName,bookAuthor,bookCaterogies,bookPublisher;
+
+    public Book(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public Book(int bookID, int bookPrice, int bookPages, String bookName, String bookAuthor, String bookCaterogies, String bookPublisher) {
+        this.bookID = bookID;
+        this.bookPrice = bookPrice;
+        this.bookPages = bookPages;
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.bookCaterogies = bookCaterogies;
+        this.bookPublisher = bookPublisher;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public int getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(int bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public int getBookPages() {
+        return bookPages;
+    }
+
+    public void setBookPages(int bookPages) {
+        this.bookPages = bookPages;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    public void setBookPublisher(String bookPublisher) {
+        this.bookPublisher = bookPublisher;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public String getBookCaterogies() {
+        return bookCaterogies;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public void setBookCaterogies(String bookCaterogies) {
+        this.bookCaterogies = bookCaterogies;
+    }
+}
