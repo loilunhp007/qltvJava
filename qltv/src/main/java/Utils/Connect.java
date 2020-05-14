18643e9056e326952f65c218f77d9e66fe4b5be3
naq7826 <47947675+naq7826@users.noreply.github.com>
2020-05-14 18:24:58 +0700

    Updated

--- qltv/src/main/java/Utils/Connect.java
+++ qltv/src/main/java/Utils/Connect.java
@@ -2,28 +2,59 @@ package Utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import Entity.*;
+
 
 public class Connect {
                 
     String dbName="qltv";
     String user="root";
     String password="";
-    Connection connection;
+    static Connection connection;
 
-    public Connection  getConnection()throws SQLException {
+    public Connection getConnection() throws SQLException {
         try {
 
             Class.forName("com.mysql.jdbc.Driver");
-            connection= DriverManager.getConnection("jdbc:mysql://localhost/"+dbName,user,password);
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/" + dbName, user, password);
             return connection;
-        } catch (ClassNotFoundException e) {
+        } catch (final ClassNotFoundException e) {
             e.printStackTrace();
             return null;
         }
 
     }
 
+    public static int checkAccount(final String username, final String password) {
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT userName, userPassword FROM account");
+            while (rs.next()) {
+                if (username.equals(rs.getString("userName")) && password.equals(rs.getString("userPassword"))) {
+                    return 1;
+                } else
+                    return 0;
+            }
+        } catch (final Exception e) {
+            
+        }       
+        return 0;
+    }
 
+    /*public static ArrayList<Account> listAccount() {
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT userName, userPassword FROM account");            
+        } catch (Exception e) {
+            
+        }
+        return rs;       
+    }
+    */
 
 }
