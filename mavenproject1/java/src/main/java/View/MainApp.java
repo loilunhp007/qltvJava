96e3c72cf1b388cf7fb9aef4330a0cd7ac8e76e5
loilunhp007 <63707576+loilunhp007@users.noreply.github.com>
2020-06-02 17:41:37 +0700

    a

--- mavenproject1/java/src/main/java/View/MainApp.java
+++ mavenproject1/java/src/main/java/View/MainApp.java
@@ -13,7 +13,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Lending.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Lending.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Library Management");
         stage.setScene(scene);
