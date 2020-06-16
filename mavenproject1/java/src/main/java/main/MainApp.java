a9518db5385b4d99e08c612f1875da82819eb657
loilunhp007 <63707576+loilunhp007@users.noreply.github.com>
2020-06-16 14:37:18 +0700

    delete img account

--- mavenproject1/java/src/main/java/main/MainApp.java
+++ mavenproject1/java/src/main/java/main/MainApp.java
@@ -13,7 +13,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../View/StaffManagement.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../View/Account.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Library Management System");
         stage.setScene(scene);
