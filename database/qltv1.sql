7e40f2276ce42d0c3f20f9150c2d4deaef337364
naq7826 <47947675+naq7826@users.noreply.github.com>
2020-06-17 13:29:45 +0700

    sql

--- database/qltv1.sql
+++ database/qltv1.sql
@@ -1,379 +1,196 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 13, 2020 lúc 06:56 AM
--- Phiên bản máy phục vụ: 10.4.11-MariaDB
--- Phiên bản PHP: 7.4.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Cơ sở dữ liệu: `qltv`
---
-	create database qltv1 ;
-    use qltv1;
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `account`
---
-
-CREATE TABLE `account` (
-  `userID` int(11) NOT NULL,
-  `userName` varchar(50) NOT NULL,
-  `userPassword` varchar(20) NOT NULL,
-  `startDay` varchar(20) NOT NULL,
-  `outofDay` varchar(20) NOT NULL,
-  `staffID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `account`
---
-
-INSERT INTO `account` (`userID`, `userName`, `userPassword`, `startDay`, `outofDay`, `staffID`) VALUES
-(1, 'lap123', '123456', '2020-13-05', '9999-9-29', 5),
-(2, 'quan123', '123456', '2020-13-05', '9999-9-29', 4);
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `author`
---
-
-CREATE TABLE `author` (
-  `authorID` int(11) NOT NULL,
-  `authorName` varchar(50) NOT NULL,
-  `authorGender` varchar(5) NOT NULL,
-  `authorDOB` date NOT NULL,
-  `authorEmail` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `author`
---
-
-INSERT INTO `author` (`authorID`, `authorName`, `authorGender`, `authorDOB`, `authorEmail`) VALUES
-(1, 'Ernest Hemingway', 'Male', '1899-07-21', NULL),
-(2, 'Patricia Highsmith', 'Male', '1921-01-19', NULL);
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `booklending`
---
-
-CREATE TABLE `booklending` (
-  `lendID` int(11) NOT NULL,
-  `lendStudentID` int(11) NOT NULL,
-  `createDay` varchar(15) NOT NULL,
-  `setdueDay` varchar(15) NOT NULL,
-  `issued_by` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `booklending`
---
-
-INSERT INTO `booklending` (`lendID`, `lendStudentID`, `createDay`, `setdueDay`, `issued_by`) VALUES
-(1, 1, '2020-13-05', '2020-03-20', 4),
-(2, 2, '2020-13-05', '2020-03-20', 5);
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `book_detail`
---
-
-CREATE TABLE `book_detail` (
-  `bookID` int(11) NOT NULL,
-  `bookName` varchar(50) NOT NULL,
-  `bookAuthorID` int(11) NOT NULL,
-  `bookCategoryID` int(11) NOT NULL,
-  `bookPublisher` varchar(50) NOT NULL,
-  `bookPrice` int(11) NOT NULL,
-  `bookPages` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `book_detail`
---
-
-INSERT INTO `book_detail` (`bookID`, `bookName`, `bookAuthorID`, `bookCategoryID`, `bookPublisher`, `bookPrice`, `bookPages`) VALUES
-(1, 'The Old Man and the Sea', 1, 1, '1952', 300000, 300),
-(2, 'The Talented Mr. Ripley ', 2, 1, '1955', 257000, 168);
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `categories`
---
-
-CREATE TABLE `categories` (
-  `categoryID` int(11) NOT NULL,
-  `categoryName` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `categories`
---
-
-INSERT INTO `categories` (`categoryID`, `categoryName`) VALUES
-(1, 'Literature'),
-(2, 'Math');
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `lending_detail`
---
-
-CREATE TABLE `lending_detail` (
-  `lendID` int(11) NOT NULL,
-  `bookID` int(11) NOT NULL,
-  `lendQuantity` int(11) NOT NULL,
-  `dueDay` varchar(15) NOT NULL,
-  `lendStatus` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `lending_detail`
---
-
-INSERT INTO `lending_detail` (`lendID`, `bookID`, `lendQuantity`, `dueDay`, `lendStatus`) VALUES
-(1, 1, 1, '2020-5-19', 'Lending'),
-(2, 2, 1, '2020-5-19', 'Lending');
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `role`
---
-
-CREATE TABLE `role` (
-  `roleID` int(11) NOT NULL,
-  `roleName` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `role`
---
-
-INSERT INTO `role` (`roleID`, `roleName`) VALUES
-(1, 'Staff'),
-(2, 'Admin');
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `staff`
---
-
-CREATE TABLE `staff` (
-  `staffID` int(11) NOT NULL,
-  `staffName` varchar(50) NOT NULL,
-  `staffdob` date NOT NULL,
-  `staffAddr` varchar(50) NOT NULL,
-  `staffGender` varchar(5) NOT NULL,
-  `staffPhone` varchar(12) NOT NULL,
-  `staff_roleID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `staff`
---
-
-INSERT INTO `staff` (`staffID`, `staffName`, `staffdob`, `staffAddr`, `staffGender`, `staffPhone`, `staff_roleID`) VALUES
-(4, 'Quan', '2000-12-01', 'Ho Chi Minh city', 'Male', '0123456789', 1),
-(5, 'Lap', '2000-12-01', 'Ha Noi city', 'Male', '9876543210', 2);
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `student`
---
-
-CREATE TABLE `student` (
-  `studentID` int(11) NOT NULL,
-  `studentName` varchar(50) NOT NULL,
-  `studenDOB` date NOT NULL,
-  `studenEmail` varchar(50) NOT NULL,
-  `studentClass` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `student`
---
-
-INSERT INTO `student` (`studentID`, `studentName`, `studenDOB`, `studenEmail`, `studentClass`) VALUES
-(1, 'Joe', '1999-03-24', '', 'AK181'),
-(2, 'Peter', '1998-12-17', '', 'AK182');
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`userID`),
-  ADD KEY `account_staff_FK` (`staffID`);
-
---
--- Chỉ mục cho bảng `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`authorID`);
-
---
--- Chỉ mục cho bảng `booklending`
---
-ALTER TABLE `booklending`
-  ADD PRIMARY KEY (`lendID`),
-  ADD KEY `lending_student` (`lendStudentID`),
-  ADD KEY `lend_lendingStaff` (`issued_by`);
-
---
--- Chỉ mục cho bảng `book_detail`
---
-ALTER TABLE `book_detail`
-  ADD PRIMARY KEY (`bookID`),
-  ADD KEY `book_author_FK` (`bookAuthorID`),
-  ADD KEY `book_category_FK` (`bookCategoryID`);
-
---
--- Chỉ mục cho bảng `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`categoryID`);
-
---
--- Chỉ mục cho bảng `lending_detail`
---
-ALTER TABLE `lending_detail`
-  ADD PRIMARY KEY (`lendID`,`bookID`),
-  ADD KEY `book_lending_FK` (`bookID`);
-
---
--- Chỉ mục cho bảng `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`roleID`);
-
---
--- Chỉ mục cho bảng `staff`
---
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`staffID`),
-  ADD KEY `staff_role_FK` (`staff_roleID`);
-
---
--- Chỉ mục cho bảng `student`
---
-ALTER TABLE `student`
-  ADD PRIMARY KEY (`studentID`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `account`
---
-ALTER TABLE `account`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT cho bảng `author`
---
-ALTER TABLE `author`
-  MODIFY `authorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT cho bảng `booklending`
---
-ALTER TABLE `booklending`
-  MODIFY `lendID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT cho bảng `book_detail`
---
-ALTER TABLE `book_detail`
-  MODIFY `bookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT cho bảng `categories`
---
-ALTER TABLE `categories`
-  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT cho bảng `role`
---
-ALTER TABLE `role`
-  MODIFY `roleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT cho bảng `staff`
---
-ALTER TABLE `staff`
-  MODIFY `staffID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT cho bảng `student`
---
-ALTER TABLE `student`
-  MODIFY `studentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Các ràng buộc cho các bảng đã đổ
---
-
---
--- Các ràng buộc cho bảng `account`
---
-ALTER TABLE `account`
-  ADD CONSTRAINT `account_staff_FK` FOREIGN KEY (`staffID`) REFERENCES `staff` (`staffID`);
-
---
--- Các ràng buộc cho bảng `booklending`
---
-ALTER TABLE `booklending`
-  ADD CONSTRAINT `lend_lendingStaff` FOREIGN KEY (`issued_by`) REFERENCES `staff` (`staffID`),
-  ADD CONSTRAINT `lending_student` FOREIGN KEY (`lendStudentID`) REFERENCES `student` (`studentID`);
-
---
--- Các ràng buộc cho bảng `book_detail`
---
-ALTER TABLE `book_detail`
-  ADD CONSTRAINT `book_author_FK` FOREIGN KEY (`bookAuthorID`) REFERENCES `author` (`authorID`),
-  ADD CONSTRAINT `book_category_FK` FOREIGN KEY (`bookCategoryID`) REFERENCES `categories` (`categoryID`);
-
---
--- Các ràng buộc cho bảng `lending_detail`
---
-ALTER TABLE `lending_detail`
-  ADD CONSTRAINT `book_lending_FK` FOREIGN KEY (`bookID`) REFERENCES `book_detail` (`bookID`),
-  ADD CONSTRAINT `lend_lending_FK` FOREIGN KEY (`lendID`) REFERENCES `booklending` (`lendID`);
-
---
--- Các ràng buộc cho bảng `staff`
---
-ALTER TABLE `staff`
-  ADD CONSTRAINT `staff_role_FK` FOREIGN KEY (`staff_roleID`) REFERENCES `role` (`roleID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema qltv1
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema qltv1
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `qltv1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `qltv1` ;
+
+-- -----------------------------------------------------
+-- Table `qltv1`.`role`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qltv1`.`role` (
+  `roleID` INT NOT NULL AUTO_INCREMENT,
+  `roleName` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`roleID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `qltv1`.`staff`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qltv1`.`staff` (
+  `staffID` INT NOT NULL AUTO_INCREMENT,
+  `staffName` VARCHAR(50) NOT NULL,
+  `staffdob` DATE NOT NULL,
+  `staffAddr` VARCHAR(100) NOT NULL,
+  `staffGender` VARCHAR(6) NOT NULL,
+  `staffPhone` INT NOT NULL,
+  `staff_roleID` INT NOT NULL,
+  `staffSalary` INT NOT NULL,
+  PRIMARY KEY (`staffID`),
+  INDEX `staff_role_FK` (`staff_roleID` ASC) VISIBLE,
+  CONSTRAINT `staff_role_FK`
+    FOREIGN KEY (`staff_roleID`)
+    REFERENCES `qltv1`.`role` (`roleID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `qltv1`.`account`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qltv1`.`account` (
+  `userID` INT NOT NULL AUTO_INCREMENT,
+  `userName` VARCHAR(50) NOT NULL,
+  `userPassword` VARCHAR(20) NOT NULL,
+  `startDay` VARCHAR(20) NOT NULL,
+  `outofDay` VARCHAR(20) NOT NULL,
+  `staffID` INT NOT NULL,
+  PRIMARY KEY (`userID`),
+  INDEX `account_staff_FK` (`staffID` ASC) VISIBLE,
+  CONSTRAINT `account_staff_FK`
+    FOREIGN KEY (`staffID`)
+    REFERENCES `qltv1`.`staff` (`staffID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `qltv1`.`author`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qltv1`.`author` (
+  `authorID` INT NOT NULL AUTO_INCREMENT,
+  `authorName` VARCHAR(50) NOT NULL,
+  `authorGender` VARCHAR(5) NOT NULL,
+  `authorDOB` DATE NOT NULL,
+  `authorEmail` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`authorID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `qltv1`.`categories`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qltv1`.`categories` (
+  `categoryID` INT NOT NULL AUTO_INCREMENT,
+  `categoryName` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`categoryID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `qltv1`.`book`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qltv1`.`book` (
+  `bookID` INT NOT NULL AUTO_INCREMENT,
+  `bookName` VARCHAR(50) NOT NULL,
+  `bookAuthorID` INT NOT NULL,
+  `bookCategoryID` INT NOT NULL,
+  `bookPublisher` VARCHAR(50) NOT NULL,
+  `bookPrice` INT NOT NULL,
+  `bookPages` INT NOT NULL,
+  PRIMARY KEY (`bookID`),
+  INDEX `book_author_FK` (`bookAuthorID` ASC) VISIBLE,
+  INDEX `book_category_FK` (`bookCategoryID` ASC) VISIBLE,
+  CONSTRAINT `book_author_FK`
+    FOREIGN KEY (`bookAuthorID`)
+    REFERENCES `qltv1`.`author` (`authorID`),
+  CONSTRAINT `book_category_FK`
+    FOREIGN KEY (`bookCategoryID`)
+    REFERENCES `qltv1`.`categories` (`categoryID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `qltv1`.`student`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qltv1`.`student` (
+  `studentID` INT NOT NULL AUTO_INCREMENT,
+  `studentName` VARCHAR(50) NOT NULL,
+  `studenDOB` DATE NOT NULL,
+  `studenEmail` VARCHAR(50) NOT NULL,
+  `studentClass` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`studentID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `qltv1`.`booklending`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qltv1`.`booklending` (
+  `lendID` INT NOT NULL AUTO_INCREMENT,
+  `lendStudentID` INT NOT NULL,
+  `createDay` VARCHAR(15) NOT NULL,
+  `setdueDay` VARCHAR(15) NOT NULL,
+  `issued_by` INT NOT NULL,
+  PRIMARY KEY (`lendID`),
+  INDEX `lending_student` (`lendStudentID` ASC) VISIBLE,
+  INDEX `lend_lendingStaff` (`issued_by` ASC) VISIBLE,
+  CONSTRAINT `lend_lendingStaff`
+    FOREIGN KEY (`issued_by`)
+    REFERENCES `qltv1`.`staff` (`staffID`),
+  CONSTRAINT `lending_student`
+    FOREIGN KEY (`lendStudentID`)
+    REFERENCES `qltv1`.`student` (`studentID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `qltv1`.`lending_detail`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qltv1`.`lending_detail` (
+  `lendID` INT NOT NULL,
+  `bookID` INT NOT NULL,
+  `lendQuantity` INT NOT NULL,
+  `dueDay` VARCHAR(15) NOT NULL,
+  `lendStatus` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`lendID`, `bookID`),
+  INDEX `book_lending_FK` (`bookID` ASC) VISIBLE,
+  CONSTRAINT `book_lending_FK`
+    FOREIGN KEY (`bookID`)
+    REFERENCES `qltv1`.`book` (`bookID`),
+  CONSTRAINT `lend_lending_FK`
+    FOREIGN KEY (`lendID`)
+    REFERENCES `qltv1`.`booklending` (`lendID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
